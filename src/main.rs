98b3d4e82b38a46f5209f216d6caa5bfd504c72b
AppleFlavored <boostelixir@gmail.com>
2021-11-26 16:04:30 -0500

    small cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
-use xcb::x;
+use xcb::{Event, x};
 
 fn main() -> xcb::Result<()> {
     // TODO: load configuration
 
     let (conn, screen_num) = xcb::Connection::connect(None)?;
+
     let setup = conn.get_setup();
     let screen = setup
         .roots()
         .nth(screen_num as usize)
         .unwrap();
 
-    let values = [x::Cw::EventMask(
-        x::EventMask::SUBSTRUCTURE_NOTIFY | x::EventMask::SUBSTRUCTURE_REDIRECT)];
+    let cookie = conn.send_request_checked(&x::ChangeWindowAttributes {
+        window: screen.root(),
+        value_list: &[
+            x::Cw::EventMask(x::EventMask::SUBSTRUCTURE_NOTIFY | x::EventMask::SUBSTRUCTURE_REDIRECT)
+        ]
+    });
 
-    // T
-    let cookie = conn.send_request_checked(
-        &x::ChangeWindowAttributes {
-            window: screen.root(),
-            value_list: &values
-        });
-    conn.check_request(cookie)?;
+    if let Err(_) = conn.check_request(cookie) {
+        println!("Another window manager is already running.");
+        std::process::exit(1);
+    }
 
     loop {
         match conn.wait_for_event()? {
-            xcb::Event::X(e) => match e {
-                // TODO: Handle X events
+            Event::X(e) => match e {
+                x::Event::MapRequest(e) => {
+                    println!("{:?}", e.window());
+                }
                 _ => {},
             }
             _ => {},
